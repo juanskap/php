b2aebb27f5380dda30304766a9e9bfce1b4dfc84
¨juan¨ <juanccrrll@gmail.com>
2025-09-02 10:19:29 -0500

    variables

---
+++ holamundo.php
@@ -0,0 +1,25 @@
+
+    <?php
+    $nombre = "Juan"; //string o cadena 
+    $edad= 30;          //entero o integer
+    $altura = 1.75;    //decimal o float
+    $es_estudiante = true; //booleano o boolean condicional si es verdadero o falso
+    $nada = 0;       //null o nulo
+   $ciudad = "Madrid";
+
+      echo "nombre: $nombre (" . gettype ($nombre). ")\n";
+     echo "nombre: $edad (" . gettype ($edad). ")\n";
+      echo "nombre: $altura (" . gettype ($altura). ")\n";
+  
+  
+  
+  
+  
+      // var_dump($edad);
+   // echo "Hola, mi nombre es $nombre, tengo $edad años y vivo en $ciudad.";
+    ?>
+
+
+
+
+
